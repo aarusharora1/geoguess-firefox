e689a61aa088a692c634370b58be33715aacb969
Aarush Arora <rush.arora@gmail.com>
2022-07-26 21:36:25 -0500

    parse works

--- br-script.js
+++ br-script.js
@@ -1,25 +1,33 @@
 function listener(details) {
-    console.log("peepee");
-    let filter = browser.webRequest.filterResponseData(details.requestId);
-    let decoder = new TextDecoder("utf-8");
-    let encoder = new TextEncoder();
-  
-    filter.ondata = event => {
-      let str = decoder.decode(event.data, {stream: true});
-      // Just change any instance of Example in the HTTP response
-      // to WebExtension Example.
-        console.log(str);
-        
-      filter.close();
+  console.log("peepee");
+  let filter = browser.webRequest.filterResponseData(details.requestId);
+  let decoder = new TextDecoder("utf-8");
+  let encoder = new TextEncoder();
+
+  filter.ondata = (event) => {
+    let str = decoder.decode(event.data, { stream: true });
+    // Just change any instance of Example in the HTTP response
+    // to WebExtension Example.
+    //console.log(str);
+    const newString = str.substring(str.indexOf("(") + 1, str.length-1);
+    //console.log(newString);
+    const parsed = JSON.parse(newString);
+    console.log(parsed);
+    if (parsed) {
+        if (parsed[1]) {
+            
+        }
     }
     filter.close();
-  
-    return {};
-  }
-  browser.webRequest.onBeforeRequest.addListener(
-    listener,
-    {urls: ["*://*.maps.googleapis.com/*"], },
-    ["blocking"]
-  );
+  };
+  filter.close();
 
-  console.log("dkjgd");
+  return {};
+}
+browser.webRequest.onBeforeRequest.addListener(
+  listener,
+  { urls: ["*://*.maps.googleapis.com/maps/api/js*"] },
+  ["blocking"]
+);
+
+console.log("dkjgd");
