e1fe80beee8a847ccb1aaca8b83020078d523d75
Aarush Arora <rush.arora@gmail.com>
2022-07-26 21:06:01 -0500

    messed it up

--- br-script.js
+++ br-script.js
@@ -3,23 +3,33 @@ function listener(details) {
     let filter = browser.webRequest.filterResponseData(details.requestId);
     let decoder = new TextDecoder("utf-8");
     let encoder = new TextEncoder();
-  
+
     filter.ondata = event => {
-      let str = decoder.decode(event.data, {stream: true});
-      // Just change any instance of Example in the HTTP response
-      // to WebExtension Example.
+        let str = decoder.decode(event.data, { stream: true });
+        // Just change any instance of Example in the HTTP response
+        // to WebExtension Example.
         console.log(str);
-        
-      filter.close();
+ 
+    //    const newThing = str.substring(str.indexOf("(") + 1);
+    //    console.log(newThing);
+    //    const parsed = JSON.parse(newThing);
+    //    console.log(parsed);
+    //     if (str[1]) {
+    //         if (str[1][0][5]) {
+    //             console.log(str[1][0][5][0][1][0]);
+    //         }
+    //     }
+        filter.write(encoder.encode(str));
+        filter.close();
     }
     filter.close();
-  
-    return {};
-  }
-  browser.webRequest.onBeforeRequest.addListener(
-    listener,
-    {urls: ["*://*.maps.googleapis.com/*"], },
-    ["blocking"]
-  );
 
-  console.log("dkjgd");
+    return {};
+}
+browser.webRequest.onBeforeRequest.addListener(
+    listener,
+    { urls: ["*://*.maps.googleapis.com/*"], },
+    ["blocking"]
+);
+
+console.log("dkjgd");
