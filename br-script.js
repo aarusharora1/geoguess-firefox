6fecf214306409d322291cb277c58b89a2d3bdf9
Aarush Arora <rush.arora@gmail.com>
2022-07-28 12:13:43 -0500

    working but laggy

--- br-script.js
+++ br-script.js
@@ -6,28 +6,68 @@ function listener(details) {
 
   filter.ondata = (event) => {
     let str = decoder.decode(event.data, { stream: true });
-    // Just change any instance of Example in the HTTP response
-    // to WebExtension Example.
-    //console.log(str);
-    const newString = str.substring(str.indexOf("(") + 1, str.length-1);
-    //console.log(newString);
-    const parsed = JSON.parse(newString);
-    console.log(parsed);
-    if (parsed) {
-        if (parsed[1]) {
-            
-        }
+
+    try {
+      const parsed = JSON.parse(str);
+      console.log(parsed);
+      if (parsed["gameLobbyId"]) {
+        console.log(parsed["gameLobbyId"]);
+        browser.webRequest.onBeforeRequest.addListener(
+          listenerTwo,
+          {
+            urls: [
+              "*://game-server.geoguessr.com/api/battle-royale/" +
+                parsed["gameLobbyId"] +
+                "/*",
+            ],
+          },
+          ["blocking"]
+        );
+      }
+    } catch {
+      console.error("error parsing");
     }
+
+    filter.write(encoder.encode(str));
     filter.close();
   };
   filter.close();
 
   return {};
 }
+function listenerTwo(details) {
+  console.log("poopoo");
+  let filter = browser.webRequest.filterResponseData(details.requestId);
+  let decoder = new TextDecoder("utf-8");
+  let encoder = new TextEncoder();
+
+  filter.ondata = (event) => {
+    let str = decoder.decode(event.data, { stream: true });
+    //console.log(str);
+    try {
+      const parsed = JSON.parse(str);
+      console.log(parsed);
+      if (parsed["rounds"]) {
+        //console.log(parsed["rounds"]);
+        const currentRound = parsed["rounds"][parsed["rounds"].length - 1];
+        if (currentRound) {
+          console.log(currentRound);
+          console.log(currentRound["lat"]);
+          console.log(currentRound["lng"]);
+        }
+      }
+    } catch {
+      console.error("error parsing");
+    }
+    filter.write(encoder.encode(str));
+    filter.close();
+  };
+  filter.close();
+}
 browser.webRequest.onBeforeRequest.addListener(
   listener,
-  { urls: ["*://*.maps.googleapis.com/maps/api/js*"] },
+  { urls: ["*://game-server.geoguessr.com/api/lobby/join-random*"] },
   ["blocking"]
 );
 
-console.log("dkjgd");
+console.log("loaded");
