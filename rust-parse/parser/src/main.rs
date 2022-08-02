b7727ed19dcb9f5e98cac9746940554508dd00e4
Aarush Arora <rush.arora@gmail.com>
2022-08-02 00:03:57 -0500

    Connection to MongoDB successful

--- rust-parse/parser/src/main.rs
+++ rust-parse/parser/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
-}
+mod secret;
+use mongodb::{bson::doc, options::ClientOptions, Client};
+#[tokio::main]
+    async fn main() -> mongodb::error::Result<()> {
+        let client_options = ClientOptions::parse(
+            secret::secret(),
+        )
+        .await?;
+        let client = Client::with_options(client_options)?;
+        let geoguessr = client.database("geoguessr");
+        for collection_name in geoguessr.list_collection_names(None).await? {
+            println!("{}", collection_name);
+        }
+
+        Ok(())
+    }
