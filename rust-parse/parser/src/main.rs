171f3a800d7237ee2235ac116ea9e59c818b8622
Aarush Arora <rush.arora@gmail.com>
2022-08-02 17:30:23 -0500

    Updated Rust things to include successful pull of information from MongoDB. Can pull latitude and longitude as intended. TryStream is causing a panic, which should be resolved.

--- rust-parse/parser/src/main.rs
+++ rust-parse/parser/src/main.rs
@@ -1,16 +1,70 @@
 mod secret;
-use mongodb::{bson::doc, options::ClientOptions, Client};
+use mongodb::{ options::ClientOptions, Client   };
+use mongodb::bson::{ Document};
+use mongodb::Collection;
+use futures::stream::{ TryStreamExt};
+//use serde::{Serialize, Deserialize};
+//use std::sync::Arc; 
+
+struct Coordinates {
+    lat: f64,
+    lng: f64, 
+}
+
+
 #[tokio::main]
     async fn main() -> mongodb::error::Result<()> {
+        //std::env::set_var("RUST_BACKTRACE", "1");
+
         let client_options = ClientOptions::parse(
             secret::secret(),
         )
         .await?;
         let client = Client::with_options(client_options)?;
         let geoguessr = client.database("geoguessr");
-        for collection_name in geoguessr.list_collection_names(None).await? {
-            println!("{}", collection_name);
+
+        let games  = geoguessr.collection::<Document>("games");
+
+
+        let mut all_documents = Collection::find(&games, None, None).await?;
+
+        // TryStream uses try_next() and iterates over Result<Option<T>>
+        while let Some(doc) = all_documents.try_next().await? {
+            let deserialized = all_documents.deserialize_current()?;
+            
+            println!(" " );
+            let mut game_id : String = String::from("");
+            let mut document_result : bson::Document = bson::Document::new();
+            let stored_document = bson::Document::get_document(&deserialized, String::from("document"));
+
+            match stored_document {
+                Ok(v) => {document_result=bson::Document::clone(v);},
+                Err(e) => println!("{}", e),
+            }         
+            let game_id_result = bson::Document::get_str(&document_result, String::from("gameid"));
+            match game_id_result {
+                Ok(v) => {game_id = String::from(v);},
+                Err(e) => {println!("{}", e);}
+            }   
+            println!("{}", game_id);
+            println!(" ");
+            let sub_doc = bson::Document::get_array(&document_result, game_id).unwrap();
+            //println!("{:?}", sub_doc);   
+            for element in sub_doc {
+                let docDoc : bson::Document = bson::to_document(&element).unwrap();
+                let lat = bson::Document::get_f64(&docDoc, String::from("lat")).unwrap();
+                let lng = bson::Document::get_f64(&docDoc, String::from("lng")).unwrap();
+                println!("{:?}", lat);
+                println!("{:?}", lng);
+                let point = Coordinates {
+                    lat: lat,
+                    lng: lng,
+                };
+            }
+            
         }
+        // TryStream uses try_collect() and collects into a Result<Vec<T>>
+        let v: Vec<_> = all_documents.try_collect().await?;    
 
         Ok(())
     }
